61f31eaccee1030b216fabb533f7479fa58dd51a
dh0369 <ggasa09@gmail.com>
2023-11-07 14:04:23 +0900

    change to normalize value of cifar10

--- train.py
+++ train.py
@@ -12,8 +12,8 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 def dataset(batch_size):
     transform = transforms.Compose([transforms.ToTensor(), 
-                                    transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                                        std=[0.229, 0.224, 0.225])])
+                                    transforms.Normalize(mean=[0.4914, 0.4822, 0.4465],
+                                                        std=[0.247, 0.243, 0.261])])
 
     trainset = datasets.CIFAR10(root='./data', train=True,
                                 download=True, transform=transform)
